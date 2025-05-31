fee3a2dd5afad017c498d443289eb5e903348cee
hhusein-alt <gm1080rr@gmail.com>
2025-05-31 16:41:31 +0400

    Configure for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/looplast',
   images: {
     unoptimized: true,
   },
-  basePath: '/loop',
-};
+}
 
-export default nextConfig;
+export default nextConfig
